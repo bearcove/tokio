df01fd224dbaa60c76df5fb4422bea572fcbd92c
Amos Wenger <amos@bearcove.net>
2024-07-12 12:50:26 +0200

    try 'just using symbols'

--- tokio/src/macros/thread_local.rs
+++ tokio/src/macros/thread_local.rs
@@ -20,6 +20,39 @@ macro_rules! tokio_thread_local {
     }
 }
 
+#[cfg(all(not(all(loom, test)), feature = "export-globals"))]
+macro_rules! tokio_thread_local {
+    ($(#[$attrs:meta])* $vis:vis static $name:ident: $ty:ty = const { $expr:expr } $(;)?) => {
+        tokio_thread_local! {
+            $(#[$attrs])*
+            $vis static $name: $ty = $expr;
+        }
+    };
+
+    ($(#[$attrs:meta])* $vis:vis static $name:ident: $ty:ty = $expr:expr $(;)?) => {
+        ::std::thread_local! {
+            $(#[$attrs])*
+            pub static $name: $ty = $expr;
+        }
+
+        mod screw_it {
+            struct MyLocalKey<T: 'static> {
+                inner: unsafe fn(Option<&mut Option<T>>) -> Option<&'static T>,
+            }
+
+            #[no_mangle]
+            static $name: MyLocalKey<()> = MyLocalKey {
+                inner: |v| {
+                    unsafe {
+                        let lk = std::mem::transmute::<_, MyLocalKey<()>>(super::$name);
+                        (lk.inner)(v)
+                    }
+                }
+            };
+        }
+    };
+}
+
 #[cfg(all(not(all(loom, test)), feature = "import-globals"))]
 macro_rules! tokio_thread_local {
     ($(#[$attrs:meta])* $vis:vis static $name:ident: $ty:ty = const { $expr:expr } $(;)?) => {
@@ -33,30 +66,7 @@ macro_rules! tokio_thread_local {
         extern "C" {
             #[link_name = concat!(stringify!($name), "_THUNK")]
             #[allow(improper_ctypes)]
-            $vis static mut $name: ::std::thread::LocalKey<$ty>;
+            $vis static $name: ::std::thread::LocalKey<$ty>;
         }
-    };
-}
-
-#[cfg(all(not(all(loom, test)), feature = "export-globals"))]
-macro_rules! tokio_thread_local {
-    ($(#[$attrs:meta])* $vis:vis static $name:ident: $ty:ty = const { $expr:expr } $(;)?) => {
-        tokio_thread_local! {
-            $(#[$attrs])*
-            $vis static $name: $ty = $expr;
-        }
-    };
-
-    ($(#[$attrs:meta])* $vis:vis static $name:ident: $ty:ty = $expr:expr $(;)?) => {
-        ::std::thread_local! {
-            $(#[$attrs])*
-            static $name: $ty = $expr;
-        }
-
-        #[no_mangle]
-        #[allow(improper_ctypes)]
-        $vis static $name: ::std::thread::LocalKey<$ty> = {
-            ::std::thread::LocalKey::new(|| $name.with(|v| unsafe { ::std::ptr::read(v) }))
-        };
     };
 }
