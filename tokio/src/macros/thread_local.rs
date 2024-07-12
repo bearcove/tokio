a311235491f33c8dcbdd10885a2c2d138267bfc6
Amos Wenger <amos@bearcove.net>
2024-07-12 14:44:21 +0200

    Use rubicon

--- tokio/src/macros/thread_local.rs
+++ tokio/src/macros/thread_local.rs
@@ -10,69 +10,9 @@ macro_rules! tokio_thread_local {
     ($($tts:tt)+) => { loom::thread_local!{ $($tts)+ } }
 }
 
-#[cfg(all(
-    not(all(loom, test)),
-    not(any(feature = "import-globals", feature = "export-globals"))
-))]
+#[cfg(not(all(loom, test)))]
 macro_rules! tokio_thread_local {
     ($($tts:tt)+) => {
-        ::std::thread_local!{ $($tts)+ }
+        ::rubicon::thread_local!{ $($tts)+ }
     }
-}
-
-#[cfg(all(not(all(loom, test)), feature = "export-globals"))]
-macro_rules! tokio_thread_local {
-    ($(#[$attrs:meta])* $vis:vis static $name:ident: $ty:ty = const { $expr:expr } $(;)?) => {
-        tokio_thread_local! {
-            $(#[$attrs])*
-            $vis static $name: $ty = $expr;
-        }
-    };
-
-    ($(#[$attrs:meta])* $vis:vis static $name:ident: $ty:ty = $expr:expr $(;)?) => {
-        ::std::thread_local! {
-            $(#[$attrs])*
-            $vis static $name: $ty = $expr;
-        }
-
-        #[allow(non_snake_case)]
-        mod $name {
-            struct MyLocalKey<T: 'static> {
-                inner: unsafe fn(Option<&mut Option<T>>) -> Option<&'static T>,
-            }
-
-            #[no_mangle]
-            static $name: MyLocalKey<()> = MyLocalKey {
-                inner: |v| {
-                    unsafe {
-                        let lk = std::mem::transmute::<_, MyLocalKey<()>>(super::$name);
-                        (lk.inner)(v)
-                    }
-                }
-            };
-        }
-    };
-}
-
-#[cfg(all(not(all(loom, test)), feature = "import-globals"))]
-macro_rules! tokio_thread_local {
-    ($(#[$attrs:meta])* $vis:vis static $name:ident: $ty:ty = const { $expr:expr } $(;)?) => {
-        tokio_thread_local! {
-            $(#[$attrs])*
-            $vis static $name: $ty = $expr;
-        }
-    };
-
-    ($(#[$attrs:meta])* $vis:vis static $name:ident: $ty:ty = $expr:expr $(;)?) => {
-        #[allow(non_snake_case)]
-        mod $name {
-            extern "C" {
-                #[link_name = stringify!($name)]
-                #[allow(improper_ctypes)]
-                pub(super) static LK: ::std::thread::LocalKey<()>;
-            }
-        }
-
-        static $name: &'static ::std::thread::LocalKey<$ty> = unsafe { std::mem::transmute(&$name::LK) };
-    };
 }
